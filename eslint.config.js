164a329e5ba696fc879ce0cd552372657eb20dd8
James Sawle <jamessawle@hotmail.com>
2024-03-01 12:52:04 +0000

    Remove playground folder as not adding value

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,8 @@ import prettierConfig from 'eslint-config-prettier'
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
   {
-    ignores: ['dist/**/*'],
-  },
-  prettierConfig
+    ignores: ['/playground/**/*.ts']
+  }
 )
--- playground/add.ts
+++
@@ -1,3 +0,0 @@
-import { add } from '../dist/index.mjs'
-
-console.log('Add two numbers together - ', add(1, 2))
